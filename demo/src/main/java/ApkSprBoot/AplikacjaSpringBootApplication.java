caf806e30cd6c2c952226179d95f5d981409a25f
LeszekCzarny <66588713+LeszekCzarny@users.noreply.github.com>
2020-07-05 18:08:51 +0200

    Add files via upload

---
+++ demo/src/main/java/ApkSprBoot/AplikacjaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ApkSprBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacjaSpringBootApplication.class, args);
+	}
+
+}

7f27e3a362280ea7e91c50bcce71b3e511946046
LeszekCzarny <66588713+LeszekCzarny@users.noreply.github.com>
2020-07-05 18:10:29 +0200

    Add files via upload

---
+++ Spring Boot/src/test/java/ApkSprBoot/AplikacjaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ApkSprBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
